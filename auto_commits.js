c9ff9b58c598c96ce8581d60f902af67c73a9767
yanding.li <David.Jackson.Lyd@gmail.com>
2025-12-15 10:05:19 +0800

    feat: modify skip

--- auto_commits.js
+++ auto_commits.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 const MIN_COMMITS = 1;
 const MAX_COMMITS = 10;
-const DEFAULT_SKIP_PROB = 0.2; // 默认概率为0（不跳过）
+const DEFAULT_SKIP_PROB = 0.25; // 默认概率为0（不跳过）
 const WORK_START_MINUTE = 9 * 60; // 09:00 local time
 const WORK_END_MINUTE = 18 * 60; // 18:00 local time
 const COMMIT_FILE = path.resolve(__dirname, 'commit.md');
